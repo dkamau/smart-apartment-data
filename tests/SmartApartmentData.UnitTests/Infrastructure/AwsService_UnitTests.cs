519739b02554ff21e2d722d793f1b1f0e38ab8d8
Dennis Kamau <34334743+dkamau@users.noreply.github.com>
2022-01-04 14:22:54 +0300

    Configure unit tests

---
+++ tests/SmartApartmentData.UnitTests/Infrastructure/AwsService_UnitTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace SmartApartmentData.UnitTests.Infrastructure
+{
+    public class AwsService_UnitTests
+    {
+        [Fact]
+        public void Sample_Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
